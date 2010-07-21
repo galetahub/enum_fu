0cc8f11c40cc7b2f8efcf788772183341f7a182e
Igor Galeta <galeta.igor@gmail.com>
2010-07-21 15:14:26 +0300

    Fix railtie initializer in production mode

--- lib/enum_fu/railtie.rb
+++ lib/enum_fu/railtie.rb
@@ -5,7 +5,7 @@ module EnumFu
   if defined? Rails::Railtie
     require 'rails'
     class Railtie < Rails::Railtie
-      config.after_initialize do
+      config.before_initialize do
         ActiveSupport.on_load :active_record do
           ActiveRecord::Base.send :include, EnumFu::Base
         end
